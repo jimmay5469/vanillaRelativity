0c7680d3eddf22cbfdf503a11b1f8f67d5a22b07
jimmay5469 <jimmay5469@gmail.com>
2019-01-18 16:18:31 -0500

    --wip-- [skip ci]

--- index.js
+++ index.js
@@ -1,47 +1,92 @@
 let moment = require('moment');
 let momentDurationFormatSetup = require("moment-duration-format");
 
-let lastTime = Date.now();
-let rTime = 0;
-let gamma = 1;
-let time = 0;
-
 document.addEventListener('DOMContentLoaded', () => {
-  const sliderNode = document.querySelector('#speedSlider');
-  
-  const computeGamma = () => {
-    const percentOfSpeedOfLight = sliderNode.value  
-    let gamma = 1/Math.sqrt(1 - Math.pow(percentOfSpeedOfLight,2))
-    return gamma
-  }
+  const sliderNode = document.querySelector('.speedSlider');
+  (function() {
+    let lastTime = Date.now();
+    let rTime = 0;
+    let gamma = 1;
+    let time = 0;
 
-  const relativeTimeNode = document.querySelector('#relativeTime');
-  const stationaryTimeNode = document.querySelector('#stationaryTime');
-  const descriptionParagraph = document.querySelector('#sliderDescription');
-  const resetButton = document.querySelector('.reset-button');
+    const computeGamma = () => {
+      const percentOfSpeedOfLight = sliderNode.value
+      let gamma = 1/Math.sqrt(1 - Math.pow(percentOfSpeedOfLight,2))
+      return gamma
+    }
 
-  resetButton.addEventListener("click", () => {
-    rTime = 0,
-    time = 0;
-    renderTimers(0,0);
-  })
+    const relativeTimeNode = document.querySelector('.container1 .relativeTime');
+    const stationaryTimeNode = document.querySelector('.container1 .stationaryTime');
+    const descriptionParagraph = document.querySelector('.container1 .sliderDescription');
+    const resetButton = document.querySelector('.container1 .reset-button');
 
-  const renderTimers = (rTime, time) => {
-    relativeTimeNode.innerText = moment.duration(rTime).format('mm:ss');
-    stationaryTimeNode.innerText = moment.duration(time).format('mm:ss'); 
-    descriptionParagraph.innerText = sliderNode.value;
-  };
+    resetButton.addEventListener("click", () => {
+      rTime = 0,
+      time = 0;
+      renderTimers(0,0);
+    })
 
-  const tick = () => {
-    const now = Date.now();
-    const sinceLast = now - lastTime;
-    gamma = computeGamma()
-    time += sinceLast;
-    rTime += (sinceLast / gamma);
-    renderTimers( rTime, time);
-    lastTime = now;
-  };
+    const renderTimers = (rTime, time) => {
+      relativeTimeNode.innerText = moment.duration(rTime).format('mm:ss.SSS');
+      stationaryTimeNode.innerText = moment.duration(time).format('mm:ss.SSS');
+      descriptionParagraph.innerText = sliderNode.value;
+    };
 
-  setInterval(tick, 100);
+    const tick = () => {
+      const now = Date.now();
+      const sinceLast = now - lastTime;
+      gamma = computeGamma()
+      time += sinceLast;
+      rTime += (sinceLast / gamma);
+      renderTimers( rTime, time);
+      lastTime = now;
+    };
+
+    setInterval(tick, 100);
+  })();
+  (function() {
+    let lastTime = Date.now();
+    let rTime = 0;
+    let gamma = 1;
+    let time = 0;
+
+    const computeGamma = () => {
+      const percentOfSpeedOfLight = sliderNode.value
+      let gamma = 1/Math.sqrt(1 - Math.pow(percentOfSpeedOfLight,2))
+      return gamma
+    }
+
+    const relativeTimeNode = document.querySelector('.container2 .relativeTime');
+    const stationaryTimeNode = document.querySelector('.container2 .stationaryTime');
+    const descriptionParagraph = document.querySelector('.container2 .sliderDescription');
+    const resetButton = document.querySelector('.container2 .reset-button');
+
+    resetButton.addEventListener("click", () => {
+      rTime = 0,
+      time = 0;
+      renderTimers(0,0);
+    })
+
+    const renderTimers = (rTime, time) => {
+      relativeTimeNode.innerText = moment.duration(rTime).format('mm:ss.SSS');
+      stationaryTimeNode.innerText = moment.duration(time).format('mm:ss.SSS');
+      descriptionParagraph.innerText = sliderNode.value;
+    };
+
+    const tick = () => {
+      const now = Date.now();
+      const sinceLast = now - lastTime;
+      gamma = computeGamma()
+      time += sinceLast;
+      rTime += (sinceLast / gamma);
+      renderTimers( rTime, time);
+      lastTime = now;
+    };
+
+    setInterval(tick, getRandomArbitrary(100, 1000));
+    function getRandomArbitrary(min, max) {
+      return Math.random() * (max - min) + min;
+    }
+  })();
 });
 
